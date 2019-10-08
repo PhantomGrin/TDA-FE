ee1e1d2aeb639812311fa741a43477f2e20718b0
PhantomGrin <savindi.wijenayaka@gmail.com>
2019-10-08 09:59:15 +0530

    fixes

--- JS/charts.js
+++ JS/charts.js
@@ -1,21 +1,14 @@
-function drawChart() {
-    var ctx = document.getElementById('myChart').getContext('2d');
+function drawPieChart(elementID, dataArray, labelArray, colorBG, colorBorder) {
+    var ctx = document.getElementById(elementID).getContext('2d');
     var myDoughnutChart = new Chart(ctx, {
         type: 'doughnut',
         data: {
-            labels: ['Running', 'Waiting', 'Blocked'],
+            labels: labelArray,
             datasets: [{
-            data: [23, 4, 5],
-            backgroundColor: [
-                'rgba(40, 167, 69, 0.3)',
-                'rgba(255, 193, 7, 0.3)',
-                'rgba(220, 53, 69, 0.3)'
-            ],
-            borderColor: [
-                'rgba(40, 167, 69, 0.8)',
-                'rgba(255, 193, 7, 0.8)',
-                'rgba(220, 53, 69, 0.8)'
-            ],
+            data: dataArray,
+            backgroundColor: colorBG,
+            borderColor: colorBorder,
+            borderColor: colorBorder,
             borderWidth: 1
         }]
             },
@@ -28,6 +21,33 @@ function drawChart() {
             animation: {
                 animateScale: true,
                 animateRotate: true
+            }
+        }
+    });
+}
+
+function drawBarChart(elementID, dataArray, labelArray, colorBG, colorBorder) {
+    var ctx = document.getElementById(elementID).getContext('2d');
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: labelArray,
+        datasets: [
+            {
+                backgroundColor: colorBG,
+                data: dataArray
+            }
+        ]
+        },
+        options: {
+            legend: { display: false },
+            title: {display: false},
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
             }
         }
     });
