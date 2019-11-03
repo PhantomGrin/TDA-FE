15b04eafefb69e84da67b059d472cb763b73017a
PhantomGrin <savindi.wijenayaka@gmail.com>
2019-11-04 02:25:45 +0530

    sharing added

--- JS/analyze.js
+++ JS/analyze.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
               "pageLength": 5
             }
         );
+        retrieveTeamMembers();
     } catch (error) {
         console.log("Username not set")
         document.getElementById("display-name").innerHTML = "Undefined";
@@ -32,7 +33,7 @@ function displayPreviousAnalysis(array){
                     '<td class="button-group-wrapper justify-content-center justify-content-md-center">'+
                         '<div class="joption-button-group ustify-content-center justify-content-md-center">'+
                             '<button class="option-buttons view-analysis-button" data-toggle="tooltip" data-placement="top" title="View Analysis"><i class="fas fa-external-link-alt"></i></button>'+
-                            '<button class="option-buttons share-analysis-button" data-toggle="tooltip" data-placement="top" title="Share Analysis"><i class="fas fa-share-alt"></i></button>'+
+                            '<button class="option-buttons share-analysis-button" data-toggle="tooltip" data-placement="top" title="Share Analysis" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-share-alt"></i></button>'+
                             '<button class="option-buttons delete-analysis-button" data-toggle="tooltip" data-placement="top" title="Delete Analysis"><i class="fas fa-trash"></i></button>'+
                         '</div>'+
                     '</td>'+
@@ -49,15 +50,22 @@ function displayPreviousAnalysis(array){
 
 $("body").on("click", ".view-analysis-button", function(){
     var value = $(this).closest("tr").find('td')[0].innerHTML;
-    // alert(value);
     getSelectedAnalysis(value);
+});
+
+$("body").on("click", ".share-analysis-button", function(){
+    var value = $(this).closest("tr").find('td')[0].innerHTML;
+    localStorage.setItem("selected_dump", JSON.stringify(value));
+
+    $($(this).data("target")).modal("show");
+    var team = JSON.parse(localStorage.getItem('team_members'));
+
+    displayTeam(team);
 });
 
 function getSelectedAnalysis(id){
     const url = `http://localhost:8080/getresult?id=${id}`;
     var token = JSON.parse(localStorage.getItem('token'));
-
-   
 
     console.log(token['token']);
     
@@ -81,3 +89,93 @@ function getSelectedAnalysis(id){
     })
     .catch(error => console.error(error))
 }
+
+function retrieveTeamMembers(){
+    const url = `http://localhost:8080/team`;
+    var token = JSON.parse(localStorage.getItem('token'));
+    
+    fetch(url, {
+        method:"GET",
+        headers: {
+            "Authorization": "Bearer "+ token['token']
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else{
+            throw new Error('Something went wrong');
+        }
+    })
+    .then(data => {
+        localStorage.setItem("team_members", JSON.stringify(data));
+    })
+    .catch(error => console.error(error))
+}
+
+function displayTeam(array){
+    array.forEach(teamMemember => {
+        try {
+            if(teamMemember["username"]!=username){
+                var inner = document.getElementById("dropdown-content").innerHTML;
+                var html = 
+                '<li class="dropdown-item" data="'+ teamMemember["username"] +'">'+
+                    '<span class="font-weight-bold member-name">'+ teamMemember["username"] +'</span> '+
+                    '<span class="display-username"> &lt;'+ teamMemember["Firstname"] +'&gt; </span> '+
+                '</li>';
+
+                document.getElementById("dropdown-content").innerHTML = inner + html;
+            }            
+        } catch (error) {
+            console.log(error)
+        }
+        
+    });
+}
+
+$("body").on("click", ".dropdown-item", function(){
+    var value = $(this).attr('data');
+    document.getElementById("success-alert").classList.add("hidden-alert");
+    document.getElementById("btn-choice").innerHTML = value;
+});
+
+$("#modal-share-btn").click(function (e){
+    e.preventDefault();
+    
+    var token = JSON.parse(localStorage.getItem('token'));
+    var dumpId = JSON.parse(localStorage.getItem('selected_dump'));
+    var sharedMember = document.getElementById("btn-choice").innerHTML;
+
+    const url = `http://localhost:8080/share?username=${sharedMember}&id=${dumpId}`;
+    
+    fetch(url, {
+        method:"GET",
+        headers: {
+            "Authorization": "Bearer "+ token['token']
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById("success-alert").classList.remove("hidden-alert");
+            return response.json();
+        } else{
+            throw new Error('Something went wrong');
+        }
+    })
+    .then(data => {
+        console.log(data);
+        localStorage.removeItem("selected_dump")
+    })
+    .catch(error => {
+        console.error(error)
+        document.getElementById("success-alert").classList.remove("alert-success");
+        document.getElementById("success-alert").classList.add("alert-danger");
+        document.getElementById("success-alert").classList.remove("hidden-alert");
+        document.getElementById("message").innerHTML = error;
+    })
+});
+
+$("#success-close").click(function(e){
+    e.preventDefault();
+    document.getElementById("success-alert").classList.add("hidden-alert");
+});
